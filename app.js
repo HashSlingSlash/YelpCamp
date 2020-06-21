6ed6f86d31aedc184fb113643118bcc3259e692d
Michael Phaneuf <mdphaneuf@yahoo.com>
2020-06-20 23:17:43 -0700

    made entire db url env var

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const commentRoutes    = require("./routes/comments"),
       indexRoutes      = require("./routes/index");
 
 require('dotenv').config();
-mongoose.connect("mongodb+srv://mdphaneuf:" + process.env.DB_PASS + "@yelpcamp.h6whz.mongodb.net/yelp_camp?retryWrites=true&w=majority", 
+mongoose.connect(process.env.DATABASE_URL,
 { useUnifiedTopology: true, useNewUrlParser: true }).then(() =>{
   console.log("Connected to DB!");
 }).catch(err =>{
